dd565162b4669f3ee570415e505ab7500062bd3c
Alexander Mold <amold001@gold.ac.uk>
2018-01-22 11:08:22 +0000

    Updated Hello to HelloWorld

--- template/src/App.vue
+++ template/src/App.vue
@@ -13,13 +13,13 @@
 
 <script>
   {{#unless router}}
-  import Hello from './components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  import HelloWorld from '@/components/HelloWorld'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
   {{/unless}}
   export default {
     name: 'app'{{#router}}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{else}},
     components: {
-      Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+      HelloWorld{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
     }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{/router}}
     data() {
       return {
