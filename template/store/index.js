c36eb432c43bc3cd78a0d450f4cbcc33d98545c1
Sam Wray <sam@wray.pro>
2018-01-22 15:24:59 +0000

    Fixes #6

--- template/store/index.js
+++ template/store/index.js
@@ -1,22 +1,18 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import * as actions from './actions';
-import * as getters from './getters';
 // import module from './modules/module';
 
 Vue.use(Vuex);
 
 const debug = process.env.NODE_ENV !== 'production';
-if(debug) {
+if (debug) {
   console.warn(`🐱: Vuex is in STRICT mode as NODE_ENV is set to "${process.env.NODE_ENV}".\n
 All commits are syncronously watched. Any performance lag is due to this.`);
 }
 
 export default new Vuex.Store({
-  actions,
-  getters,
   modules: {
     // module
   },
-  strict: debug
+  strict: debug,
 });
